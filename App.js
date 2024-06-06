df312ad2a671a5f1cd4c95c6c03efe8f3050c5e2
AtanasovCode <a_ljupce@yahoo.com>
2024-06-06 16:12:29 +0200

    created sign in / sign up page

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native';
+import Auth from './components/Auth';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-black flex-1 pt-10">
+      <Auth />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Auth.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    AppState,
+    Alert,
+} from "react-native";
+import { supabase } from "../lib/supabase";
+import { FontAwesome } from '@expo/vector-icons';
+
+AppState.addEventListener('change', (state) => {
+    if (state === 'active') {
+        supabase.auth.startAutoRefresh()
+    } else {
+        supabase.auth.stopAutoRefresh()
+    }
+})
+
+const Auth = () => {
+
+    const [loading, setLoading] = useState(false);
+
+    return (
+        <View className="flex-1">
+            <View className="flex-row items-center justify-center gap-4 mb-16">
+                <FontAwesome name="users" size={36} color="white" />
+                <Text className="text-white font-bold text-2xl">User Management</Text>
+            </View>
+            <View className="mx-6 mb-8">
+                <TextInput
+                    placeholder="mail@example.com"
+                    placeholderTextColor="#a09d9d"
+                    className="bg-slate-800 px-4 py-2 rounded-2xl"
+                    keyboardType="email-address"
+                />
+            </View>
+            <View className="mx-6 mb-8">
+                <TextInput
+                    placeholder="Password"
+                    placeholderTextColor="#a09d9d"
+                    className="bg-slate-800 px-4 py-2 rounded-2xl text-white"
+                    keyboardType="email-address"
+                    secureTextEntry={true}
+                />
+            </View>
+            <View className="mx-6 mb-8">
+                <TouchableOpacity
+                    className="p-4 rounded-2xl bg-sky-400"
+                    disabled={loading}
+                    onPress={() => signInWithEmail()}
+                >
+                    <Text className="text-center">Sign Up</Text>
+                </TouchableOpacity>
+            </View>
+            <View className="mx-6 mb-8">
+                <TouchableOpacity
+                    className="p-4 rounded-2xl bg-gray-300"
+                    disabled={loading}
+                    onPress={() => signUpWithEmail()}
+                >
+                    <Text className="text-center">Sign In</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+}
+
+export default Auth;
