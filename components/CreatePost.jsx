bdc22fd9bba09177607105131afb65051e5ae127
AtanasovCode <a_ljupce@yahoo.com>
2024-06-11 18:42:42 +0200

    created inputs for new post

--- components/CreatePost.jsx
+++ components/CreatePost.jsx
@@ -3,19 +3,41 @@ import {
     View,
     Text,
     TextInput,
+    TouchableOpacity,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const CreatePost = ({ navigation }) => {
     return (
-        <View className="flex-1 bg-background py-16">
-            <View className="mb-12">
-                <Text className="font-bold text-2xl text-center">
-                    Create New Post
-                </Text>
+        <SafeAreaView className="flex-1 bg-background py-12 justify-between">
+            <View>
+                <View className="mb-12">
+                    <Text className="font-bold text-2xl text-center text-text">
+                        Create New Post
+                    </Text>
+                </View>
+                <View className="mx-8">
+                    <TextInput
+                        className="py-4 px-6 bg-secondary rounded-xl mb-4"
+                        placeholder="Title"
+                        placeholderTextColor={"#aaa"}
+                    />
+                    <TextInput
+                        className="py-4 px-6 bg-secondary rounded-xl text-text"
+                        placeholder="Body (optional)"
+                        placeholderTextColor={"#aaa"}
+                        multiline={true}
+                        numberOfLines={8}
+                        style={{ textAlignVertical: "top" }}
+                    />
+                </View>
             </View>
-            <View className="">
+            <View className="mx-8 mt-16">
+                <TouchableOpacity className="bg-accent p-4 rounded-xl">
+                    <Text className="text-text text-lg text-center">Create Post</Text>
+                </TouchableOpacity>
             </View>
-        </View>
+        </SafeAreaView>
     );
 }
 
